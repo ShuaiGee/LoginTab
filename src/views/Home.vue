66f58c06de26b37770514b287177fd0cab2ebace
ShuaiGee <862376350@qq.com>
2019-02-20 23:14:16 +0800

    vueTest

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <router-view /><!--将下方的li标签设置好样式，将实现与android中fragment相同的功能-->
+    <ul>
+      <li><router-link :to="{name: 'list'}">新闻列表</router-link></li>
+      <li><router-link :to="{name: 'user'}">个人中心</router-link></li>
+    </ul>
+  </div>
+
+
+
+</template>
+<script>
+
+    export default {
+        name:"Home"
+    }
+</script>
+<style scoped>
+
+</style>
---
+++ src/views/Info.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>info</div>
+ <!-- 此模块暂时没用上！-->
+
+</template>
+<script>
+
+    export default {
+      name:"Info"
+    }
+</script>
+<style scoped>
+
+</style>
